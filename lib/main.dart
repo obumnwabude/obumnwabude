166702987090b0cae95f77b8522499771046c914
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-08-13 17:24:58 +0100

    make app scrollable in landscape orientation.
    extract the home to a HomeScreen widget to permit usage of MediaQuery.
    reduce top padding in landscape mode.
    make social media buttons to have same color as headline text.
    reduce padding between headline text and social media buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -8,46 +8,60 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Obumuneme Nwabude',
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
+        ),
+        home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
   String capitalize(String str) => str[0].toUpperCase() + str.substring(1);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Obumuneme Nwabude',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ClipRRect(
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).orientation == Orientation.portrait
+                    ? 128
+                    : 32),
+            child: Center(
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(10000),
                 child: Image.asset('assets/images/obum.jpg', width: 250),
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 16.0, bottom: 32.0),
-                child: Text(
-                  'Obumuneme Nwabude',
-                  style: Theme.of(context).textTheme.headline4,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: socialLinks.map((sL) {
-                    return IconButton(
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 12),
+            child: Text(
+              'Obumuneme Nwabude',
+              style: Theme.of(context).textTheme.headline4,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: socialLinks.map((sL) {
+                  return IconButton(
                       padding: EdgeInsets.only(left: 2, right: 2),
                       icon: Icon(SocialIcons.icons[sL.icon], size: 36),
                       onPressed: () => launch(sL.link),
                       tooltip: capitalize(sL.icon),
-                    );
-                  }).toList())
-            ],
-          ),
-        ),
+                      color: Colors.grey.shade700);
+                }).toList()),
+          )
+        ],
       ),
     );
   }
