6032482db10ebcad079d0033d9e067777373c759
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-09-28 16:11:19 +0100

    setup angular material and enable theme switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, HostBinding, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { constants } from './contants';
+import { ThemingService } from './theming.service';
 
 @Component({
   selector: 'obum-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'obumnwabude';
+export class AppComponent implements OnInit {
+  themes = constants.THEMES;
+  @HostBinding('class') public cssClass = constants.DEFAULT_THEME;
+  constructor(
+    public themingService: ThemingService,
+    private appRef: ApplicationRef,
+    private overlayContainer: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    this.themingService.theme.subscribe((theme: string) => {
+      this.cssClass = theme;
+      const oCClasses = this.overlayContainer.getContainerElement().classList;
+      oCClasses.remove(...Array.from(this.themes));
+      oCClasses.add(this.cssClass);
+    });
+  }
+
+  changeTheme(): void {
+    this.cssClass =
+      this.themes.indexOf(this.cssClass) == 0 ? this.themes[1] : this.themes[0];
+    this.themingService.theme.next(this.cssClass);
+    this.appRef.tick();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
