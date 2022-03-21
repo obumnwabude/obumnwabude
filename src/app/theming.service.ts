5050885a349df141959c57dec8de88bc567a8e70
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-03-21 05:59:16 +0100

    chore: improve themingService for Server Side Rendering
    Initiliaze theme BehaviorSubject with default theme.
    Move detection of localstorage saved theme onload to constructor.
    did this because platform could only be detected after dependency injection of platformId.
    Make the code more readable by using if statements instead of chained conditionals.

--- src/app/theming.service.ts
+++ src/app/theming.service.ts
@@ -7,24 +7,29 @@ import { constants } from './contants';
   providedIn: 'root'
 })
 export class ThemingService {
-  theme = new BehaviorSubject(
-    localStorage.getItem(constants.LS_THEME_KEY)
-      ? localStorage.getItem(constants.LS_THEME_KEY)!
-      : constants.DEFAULT_THEME
-  );
+  theme = new BehaviorSubject(constants.DEFAULT_THEME);
 
   constructor(@Inject(PLATFORM_ID) platformId: any) {
-    isPlatformBrowser(platformId) &&
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches &&
-      !localStorage.getItem(constants.LS_THEME_KEY) &&
-      this.theme.next('dark_mode');
+    if (isPlatformBrowser(platformId)) {
+      this.theme.next(
+        localStorage.getItem(constants.LS_THEME_KEY)
+          ? localStorage.getItem(constants.LS_THEME_KEY)!
+          : constants.DEFAULT_THEME
+      );
 
-    isPlatformBrowser(platformId) &&
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches &&
+        !localStorage.getItem(constants.LS_THEME_KEY)
+      ) {
+        this.theme.next('dark_mode');
+      }
+
       window
         .matchMedia('(prefers-color-scheme: dark)')
         .addEventListener('change', (e) =>
           this.theme.next(e.matches ? 'dark_mode' : 'light_mode')
         );
+    }
   }
 }
