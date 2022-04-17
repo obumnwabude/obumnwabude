a12403cdfd8c63b4dcd571c2029363e75c21d9d8
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-04-17 21:43:55 +0100

    chore: update facebook icon to reflect facebook page link

--- src/app/content/index.ts
+++ src/app/content/index.ts
@@ -9,7 +9,7 @@ export const profiles = [
   { icon: 'twitter', link: 'https://twitter.com/obumnwabude' },
   { icon: 'stackoverflow', link: 'https://stackoverflow.com/users/13644299' },
   { icon: 'google', link: 'https://g.dev/obumnwabude' },
-  { icon: 'facebook', link: 'https://facebook.com/obumnwabude' },
+  { icon: 'facebook', link: 'https://facebook.com/obumnwabude.page' },
   { icon: 'instagram', link: 'https://instagram.com/obumnwabude' }
 ];
 
