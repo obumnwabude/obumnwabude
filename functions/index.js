3e8e12ca7c537ca5c3d09e13308a86dc2faaebde
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-12-22 09:33:30 +0100

    chore: update firebase package versions and deploy

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 // Increase readability in Cloud Logging
-require('firebase-functions/lib/logger/compat');
+require('firebase-functions/logger/compat');
 
 exports.ssr_obumnwabude = functions.https.onRequest(
   require('./dist/obumnwabude/server/main').app()
