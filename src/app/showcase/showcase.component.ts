4ae42b61a892e9af6cdc441ebbfc4d7e03084266
Obum (@obumnwabude) <obumnwabude@gmail.com>
2023-12-12 10:06:08 +0100

    chore: update the main profile picture

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { constants } from '../constants';
-import { ThemingService } from './../services/theming.service';
 
 @Component({
   selector: 'obum-showcase',
   templateUrl: './showcase.component.html',
-  styleUrls: ['./showcase.component.scss']
+  styleUrls: ['./showcase.component.scss'],
 })
 export class ShowcaseComponent {
   profiles = [
@@ -13,20 +11,8 @@ export class ShowcaseComponent {
     { icon: 'twitter', link: 'https://twitter.com/obumnwabude' },
     { icon: 'stackoverflow', link: 'https://stackoverflow.com/users/13644299' },
     { icon: 'facebook', link: 'https://facebook.com/obumnwabude' },
-    { icon: 'instagram', link: 'https://instagram.com/obumnwabude' }
+    { icon: 'instagram', link: 'https://instagram.com/obumnwabude' },
   ];
 
   capitalize = (str: string) => str[0].toUpperCase() + str.substring(1);
-
-  picTheme = constants.THEMES.filter(
-    (tt) => constants.DEFAULT_THEME != tt
-  )[0].split('-mode')[0];
-
-  constructor(private theming: ThemingService) {
-    this.theming.theme.subscribe((t) => {
-      this.picTheme = constants.THEMES.filter((tt) => t != tt)[0].split(
-        '-mode'
-      )[0];
-    });
-  }
 }
