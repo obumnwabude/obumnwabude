d417f707b672d8f50dd0457f3296a9874dc33b36
Obum (@obumnwabude) <obumnwabude@gmail.com>
2024-04-27 08:48:02 +0100

    feat: build animating horizontal scroll banner in home view

--- obumnwabude/src/views/HomeView.vue
+++ obumnwabude/src/views/HomeView.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import ContactMe from '@/components/ContactMe.vue';
-import IconFlower from '@/icons/IconFlower.vue';
+import IconFlower2P from '@/icons/IconFlower2P.vue';
+import IconFlower6P from '@/icons/IconFlower6P.vue';
 
+const banners = [
+  'Web3 Dev',
+  'Solana (Rust)',
+  'EVM (Solidity)',
+  'Mobile Dev',
+  'Flutter (Dart)',
+  'Frontend',
+  'Backend (NodeJS)',
+  'Technical Writing',
+  'Community',
+  'Vibes',
+];
 const years = new Date().getFullYear() - 2018;
 </script>
 
 <template>
   <div intro>
     <div>
-      <IconFlower />
+      <IconFlower2P />
       <h2>OBUMUNEME NWABUDE</h2>
       <h3>Full-Stack Blockchain, Mobile, and Web Developer.</h3>
       <p>
@@ -21,6 +34,16 @@ const years = new Date().getFullYear() - 2018;
     <div>
       <div gray></div>
       <img src="/assets/images/obum.jpg" />
+    </div>
+  </div>
+
+  <div banner>
+    <div>
+      <span flower> <IconFlower6P /></span>
+      <template v-for="banner of banners">
+        <span text>{{ banner }}</span>
+        <span flower> <IconFlower6P /></span>
+      </template>
     </div>
   </div>
 </template>
@@ -48,6 +71,47 @@ const years = new Date().getFullYear() - 2018;
   width: 100%;
 }
 
+@keyframes bannerScroll {
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-2000px);
+  }
+}
+
+[banner] {
+  background-color: var(--primary);
+  color: var(--app-bg);
+  font-size: 1.25rem;
+  margin-left: -1.5rem;
+  margin-right: -1.5rem;
+  padding: 1rem 1rem 1rem 10rem;
+  overflow-x: hidden;
+  white-space: nowrap;
+}
+
+[banner] > div {
+  align-items: center;
+  animation: bannerScroll 20s linear infinite;
+  display: flex;
+}
+
+[banner] [flower] {
+  align-items: center;
+  display: inline-flex;
+  margin-left: 2rem;
+  margin-right: 2rem;
+}
+
+[banner] [flower]:first-of-type {
+  margin-left: 0;
+}
+
+[banner] [flower]:last-of-type {
+  margin-right: 0;
+}
+
 @media (max-width: 511.98px) {
   [intro] [gray] {
     display: none;
@@ -71,6 +135,11 @@ const years = new Date().getFullYear() - 2018;
     position: absolute;
     width: 100%;
     z-index: -1;
+  }
+
+  [banner] [flower] {
+    margin-left: 3rem;
+    margin-right: 3rem;
   }
 }
 
@@ -109,6 +178,13 @@ const years = new Date().getFullYear() - 2018;
 @media (min-width: 768px) and (max-width: 1024px) {
   [intro] [gray] {
     height: 320px;
+  }
+}
+
+@media (min-width: 1024px) {
+  [banner] {
+    margin-left: -5rem;
+    margin-right: -5rem;
   }
 }
 </style>
